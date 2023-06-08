6ee9b634d1a704b8db2174cf568ba9bb926b35f8
Daniel Matei <d.matei@EDU.epsb.ca>
2023-06-08 09:49:29 -0600

    done

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
   slider.oninput = function () {
     var year = parseInt(this.value);
     title.textContent = year;
-    // yearLabel.textContent = "Wars and conflicts in " + year + ":";
+    yearLabel.textContent = "Wars and conflicts in " + year + ":";
     warContainer.textContent = getTitle(year);
   };
 });
